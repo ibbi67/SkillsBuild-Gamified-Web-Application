f3c746d98d91111105ff6a9db2b5314385738c4c
sp869 <sp869@student.le.ac.uk>
2025-02-12 13:44:11 +0000

    feature/initialise courses in componenets

---
+++ frontend/src/components/CourseList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CourseList = () => {
+    const courses = [
+      { id: 1, title: 'React for Beginners'},
+      { id: 2, title: 'Advanced JavaScript'},
+      { id: 3, title: 'Mastering Node.js'},
+      { id: 4, title: 'Python for Data Science'}
+    ];
+}
