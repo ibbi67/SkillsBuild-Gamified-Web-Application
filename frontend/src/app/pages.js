6d5406aff3a2c8335ad1a76810b241df31d99ba3
sp869 <sp869@student.le.ac.uk>
2025-02-12 13:35:10 +0000

    feature/samplecourses

---
+++ frontend/src/app/pages.js
@@ -0,0 +1,7 @@
+const courses = [
+    { id: 1, title: 'Getting started with ai' },
+    { id: 2, title: 'Advanced JavaScript' },
+    { id: 3, title: 'Mastering Node.js' },
+    { id: 4, title: 'Python for Data Science' },
+
+];
