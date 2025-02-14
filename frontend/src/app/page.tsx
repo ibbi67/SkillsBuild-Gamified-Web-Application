287cfaf8660867713df78a1d0623efeb9f832626
choco_green <choco_green@hotmail.com>
2025-02-14 14:41:56 +0000

    fix: resolve merge conflict

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import CourseList from "../components/CourseList";
 
-export default function Home() {
+export default function HomePage() {
     return (
         <div className="flex">
             <div className="m-4 grow">
                 <Navbar />
+                <CourseList />
             </div>
         </div>
     );
