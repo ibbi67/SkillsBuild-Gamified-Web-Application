1df53b5b02f0e332d8f77908fdea72fe836bdbac
Z Altaf <mza16@student.le.ac.uk>
2025-02-17 12:08:55 +0000

    feat: streak data for relevent user passed to dashboard:

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,41 @@
 "use client";
 
+import { useApi } from "@/hooks/useApi";
+import { MeRequest, MeResponse } from "@/types/auth";
+import { useEffect, useRef } from "react";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
+    const { isLoading, isError, message, fetchData, data } = useApi<
+        MeResponse,
+        MeRequest
+    >("auth/me", { method: "GET" });
+
+
+
+    const hasFetched = useRef(false);
+
+    useEffect(() => {
+        if (!hasFetched.current) {
+            hasFetched.current = true;
+            fetchData();
+        }
+    }, [fetchData]);
+
+
     return (
         <div className="flex flex-col">
             <div className="m-4 grow">
                 <Navbar />
             </div>
             <div className="mx-auto m-4 w-4/5 rounded-lg bg-white p-4 shadow-lg">
-                <h1 className="text-left font-bold md:text-2xl">Welcome Back (enter user name here)</h1>
+                <h1 className="text-left font-bold md:text-2xl">Welcome Back {data?.username}</h1>
                 {/* Add your dashboard content here */}
                 <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-1">
                     <div className="rounded-lg border p-4 shadow">
                         <h2 className="mb-2 font-bold">Your Streak</h2>
                         <div className="flex items-center justify-center gap-2">
-                            <p className="text-xl font-bold text-green-500">5</p>
+                            <p className="text-xl font-bold text-green-500">{data?.streak.streak}</p>
                             <p className="text-xl font-bold">Days</p>
                         </div>    
                     </div>
