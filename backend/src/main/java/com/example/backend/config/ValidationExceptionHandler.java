d81ab1f7e1f152d681e23eb7645f6502d74087ce
choco_green <choco_green@hotmail.com>
2025-02-18 16:09:56 +0000

    revert the state of backend

--- backend/src/main/java/com/example/backend/config/ValidationExceptionHandler.java
+++ backend/src/main/java/com/example/backend/config/ValidationExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.backend.config;
 
 import com.example.backend.domain.ApiResponse;
+import java.util.stream.Collectors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,21 +10,20 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.stream.Collectors;
-
-
 @ControllerAdvice
 public class ValidationExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         BindingResult bindingResult = ex.getBindingResult();
-        String message = bindingResult.getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.joining(", "));
+        String message = bindingResult
+            .getFieldErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.failed(HttpStatus.BAD_REQUEST.value(), message));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ApiResponse.failed(HttpStatus.BAD_REQUEST.value(), message)
+        );
     }
 }
