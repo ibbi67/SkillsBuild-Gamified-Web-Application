58357f8233dad4f162d5fe4a9f0402c0d9c92776
choco_green <choco_green@hotmail.com>
2025-02-25 16:18:52 +0000

    initial commit

---
+++ co2124/src/main/java/com/example/co2124/Co2124Application.java
@@ -0,0 +1,13 @@
+package com.example.co2124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2124Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Co2124Application.class, args);
+    }
+
+}
---
+++ co2124/src/test/java/com/example/co2124/Co2124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.co2124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2124ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
