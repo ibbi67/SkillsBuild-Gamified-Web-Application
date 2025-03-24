58ae082db345d5e9e3b93906a6427bce9ac4cb26
choco_green <choco_green@hotmail.com>
2025-03-24 01:24:06 +0000

    feat: add initial course data

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,68 @@
 package com.example.backend;
 
+import com.example.backend.course.Course;
+import com.example.backend.course.csr.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+    private final CourseRepository courseRepository;
+
+    public BackendApplication(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        initCourseData();
+    }
+
+    public void initCourseData() {
+        courseRepository.save(new Course("Project Management Fundamentals",
+                """
+                        This course introduces you to the basic principles of project management and what it takes to be a successful project manager.
+
+                        The 3.5-hour course covers:
+                        - Common project management terms
+                        - The value of project management
+                        - Project management approaches: Waterfall, Agile, and Hybrid
+                        - The role, overall responsibilities, and competencies of a project manager
+                        - The purpose of the phases of a project: Initiate and Plan, Execute, and Close
+                        - The key tasks that a project manager performs in each project phase
+                        - The overall job market and common industry certifications to consider in the project management field
+
+                        You also get to put on your project manager “hat” to respond to some interactive situations based on a project scenario.
+                        \s
+                        Opportunity to earn a credential: Complete this course to earn the Project Management Fundamentals credential, a co-creation of IBM and IPMA!
+
+                        Click Start tracking progress to enroll in this learning plan and get started!""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-14A47D1900AA",
+                240,
+                2));
+
+        courseRepository.save(new Course("AI Foundations: A Collaboration of ISTE and IBM",
+                """
+                        There's no doubt about the importance of artificial intelligence to future generations! Your job will most certainly use some type of AI. In this digital learning offering, created by ISTE and IBM especially for high school students, you'll learn the foundational concepts behind AI systems, consider the ethical implications of AI, explore applications of AI tools, and more.
+
+                        Complete five modules, including an AI Design Challenge, and pass the final assessment with a score of 80% or higher. You'll be rewarded with the AI Foundations credential.
+
+                        Click Enroll to enroll in this learning plan and get started!
+
+                        Language: English
+
+                        Teacher Tip! Be sure to check out the Facilitator's Guide and AI Design Challenge Rubric, designed especially to support this course.
+                        """,
+                "https://students.yourlearning.ibm.com/activity/PLAN-B2125F145F0E",
+                1170,
+                3));
+    }
 }
