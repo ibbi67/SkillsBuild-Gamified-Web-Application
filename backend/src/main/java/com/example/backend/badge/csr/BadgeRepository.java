1d63b1e98e9be50f4ef64214172b93406cfca4a4
I Shahid <is220@student.le.ac.uk>
2025-03-24 22:40:55 +0000

    feat: added BadgeInitializer.java and BadgeRepository.java from old branch

---
+++ backend/src/main/java/com/example/backend/badge/csr/BadgeRepository.java
@@ -0,0 +1,16 @@
+package com.example.backend.badge.csr;
+
+import com.example.backend.badge.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BadgeRepository extends JpaRepository<Badge, Integer> {
+
+    Optional<Badge> findByName(String name);
+
+    List<Badge> findByCriteriaType(String criteriaType);
+} 
