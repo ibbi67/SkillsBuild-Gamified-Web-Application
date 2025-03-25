e39a5a9f6092aaf886d0f3078bd2baac8534d788
Dannyboy21204 <153147440+Dannyboy21204@users.noreply.github.com>
2025-03-25 21:33:28 +0000

    Addition of Courses from IBM website

--- backend/src/main/java/com/example/backend/util/DevDataInitializer.java
+++ backend/src/main/java/com/example/backend/util/DevDataInitializer.java
@@ -64,6 +64,155 @@ public class DevDataInitializer {
                 "https://students.yourlearning.ibm.com/activity/PLAN-B2125F145F0E",
                 1170,
                 3));
+
+        courseRepository.save(new Course("Cloud Computing Fundamentals",
+                """
+                        This credential earner demonstrates knowledge of cloud computing, including cloud services, deployment models,
+                        virtualization, orchestration, and cloud security. The individual is aware of cloud benefits for users and businesses.
+                        The individual has a conceptual understanding of how to create a container, deploy a web app to the cloud, and
+                        analyze security in a simulated environment. The earner is aware of the job outlook in cloud computing and the
+                        skills required for success in various roles.""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-58FA14F64C9B",
+                600,
+                3));
+
+        courseRepository.save(new Course("Cybersecurity Fundamentals",
+                """
+                        This course will provide you with an introduction to cybersecurity.
+                        From the offense perspective, you will learn about cyberattackers,
+                        their tactics, social engineering, and high-profile case studies. From
+                        the defense perspective, you will learn about common approaches
+                        organizations take to prevent, detect, and respond to cyberattacks.
+                        You will also learn about career opportunities in this exciting, growing
+                        field. Finally, you’ll explore how new developments and emerging
+                        technologies, such as artificial intelligence (AI), impact cybersecurity.""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-805005E992EA",
+                420,
+                2));
+
+        courseRepository.save(new Course("Data Fundamentals",
+                """
+                Do you love to discover meaning in facts and numbers? Learn the
+                concepts and methods of data science and how its discoveries change the world.
+                Then get hands-on practice cleaning, refining, and visualizing data, in a series
+                of simulations, using IBM Watson Studio with the data refinery tool. Finish by
+                gathering tips and resources that can help you launch a great career in data science.
+
+                Complete the following required courses to earn an industry-recognized IBM SkillsBuild digital credential called Data Fundamentals:
+                """,
+                "https://students.yourlearning.ibm.com/activity/PLAN-0EC2BCEA3C39",
+                420,
+                4));
+
+        courseRepository.save(new Course("Web Development Fundamentals",
+                """
+                    Would you like opportunities to express yourself creatively on the internet?
+                    Web development is an exciting, growing field in tech. Learn the basics about the languages, tools,
+                    and processes to develop websites. Then, get hands-on practice creating an interactive task list web
+                    page in a series of simulations. Finish by gathering tips and resources that can help you launch a
+                    great career in web development.
+                    
+                    Complete the following required courses to earn an industry-recognized IBM SkillsBuild
+                    digital credential called Web Development Fundamentals:
+                    """,
+                "https://students.yourlearning.ibm.com/activity/PLAN-43A030B97485",
+                720,
+                4));
+
+        courseRepository.save(new Course("Explore Emerging Tech",
+                """
+                        Curious about tech, but not sure where to focus? You've come to the right place.
+                        
+                        In this learning plan, you'll get an intro to six emerging technologies that
+                        power today's jobs. You'll learn a little about each—like foundational concepts,
+                        terminology, and how it's used—and then you can decide where you want to go deeper.
+                        
+                        Complete all the courses in this learning plan and earn the Explore Emerging Tech credential.
+                        """,
+                "https://students.yourlearning.ibm.com/activity/PLAN-91F302DE9BBD",
+                1170,
+                2));
+
+        courseRepository.save(new Course("Open Source Origin Stories",
+                """
+                   Interested in a career in technology but not sure where to begin?
+                   Explore ways that hybrid cloud computing, artificial intelligence,
+                   and open source code reach across many tech opportunities ranging
+                   from high-powered data management to ethical AI. In three interactive
+                   superhero origin stories, you’ll learn how teams work together in
+                   open source tech and use it to transform our world.
+                   """,
+                "https://students.yourlearning.ibm.com/activity/PLAN-DEAEB6C8F149",
+                300,
+                1));
+
+        courseRepository.save(new Course("Customer Engagement Fundamentals",
+                """
+                        If you’re intrigued by the field of customer engagement,
+                        this course is for you. Learn the skills to build relationships
+                        with customers, help them solve stressful problems, and win their
+                        loyalty. In this course, you’ll discover strategies to build
+                        rapport with customers, develop communication skills to troubleshoot
+                        issues and guide customers through solutions, use scientific methods
+                        to solve problems creatively, and become a productive team member.
+                        
+                        Complete the following required modules to earn an industry-recognized
+                        IBM SkillsBuild digital credential called Customer Engagement Fundamentals:""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-F77885B0DEE9",
+                900,
+                1));
+
+        courseRepository.save(new Course("Quantum Enigmas",
+                """
+                        Embark on a journey through the key concepts of quantum computing
+                        and fascinating world of quantum enigmas. These courses are designed
+                        to provide you with a deeper understanding of quantum computing
+                        principles and techniques through the exploration and solving of
+                        intriguing quantum enigmas.\s
+                        
+                        These courses were developed in collaboration with Institut quantique de
+                        l’Université de Sherbrooke.
+                        
+                        What you’ll learn
+                        
+                        After completing Quantum Enigmas, you should be able to:
+                        
+                        - Differentiate between classical and quantum computers by
+                        identifying their fundamental differences, including computational
+                        principles and technologies
+                        - Describe the measurement process of a qubit in quantum computing
+                        and the outcomes and implications of measuring a qubit
+                        - Identify the roles and significance of quantum logic gates
+                        in quantum computing
+                        - Describe how quantum logic gates contribute to quantum
+                        information processing
+                        - Differentiate between various types of quantum gates,
+                        their unique functionalities, and application in quantum computing systems
+                        - Describe how to use the IBM quantum composer to
+                        manipulate qubits, demonstrating the process of placing a
+                        qubit into superposition, entangling qubits, and applying different
+                        types of quantum gates to control and manipulate quantum states effectively""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-63384B7F59EC",
+                480,
+                4));
+
+        courseRepository.save(new Course("User Experience Design Fundamentals",
+                """
+                        Are you passionate about creating exceptional user experiences?
+                        UX design is a dynamic and in-demand field that focuses on crafting
+                        digital products that are intuitive, user-friendly, and visually
+                        appealing. Learn about design concepts and the UX design process,
+                        from conducting research to creating wireframes and prototypes to
+                        conducting usability tests and implementing feedback. Through an
+                        example UX design case study, apply your learning of UX design
+                        principles and practices. Finish by gathering tips and resources
+                        that can help you launch a great career in UX Design.\s
+                        
+                        Complete the following required courses to earn an industry-recognized
+                        IBM SkillsBuild digital credential called User Experience Design Fundamentals:\s""",
+                "https://students.yourlearning.ibm.com/activity/PLAN-44E67AF54225",
+                720,
+                3));
     }
 
     public void initBadgeData() {
