c66a02eefea41746490070adea01f1002a5c2a71
choco_green <choco_green@hotmail.com>
2025-03-25 15:45:43 +0000

    fix: accidental removal of character

--- backend/src/main/java/com/example/backend/comment/Comment.java
+++ backend/src/main/java/com/example/backend/comment/Comment.java
@@ -23,7 +23,7 @@ public class Comment {
     private String content;
 
     @Column(nullable = false)
-    private LocalDateTime reatedAt = LocalDateTime.now();
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     @ManyToOne
     @JoinColumn(name = "person_id", nullable = false)
